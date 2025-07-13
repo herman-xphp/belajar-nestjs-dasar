88a2073610d664c6ae0f3e915be1986456787d9b
herman-xphp <hermanxphp@gmail.com>
2025-07-13 10:09:47 +0800

    HTTP Response

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,8 +1,36 @@
-import { Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Header,
+  HttpCode,
+  HttpRedirectResponse,
+  Param,
+  Post,
+  Query,
+  Redirect,
+} from '@nestjs/common';
 import { Request } from 'express';
 
 @Controller('/api/users')
 export class UserController {
+  @Get('/sample-response')
+  @Header('Content-Type', 'application/json')
+  @HttpCode(200)
+  sampleResponse(): Record<string, string> {
+    return {
+      data: 'Hello JSON',
+    };
+  }
+
+  @Get('/redirect')
+  @Redirect()
+  redirect(): HttpRedirectResponse {
+    return {
+      url: '/api/users/sample-response',
+      statusCode: 301,
+    };
+  }
+
   @Get('/hello')
   sayHallo(
     @Query('first_name') firstName: string,
